fc81b3d96e0962b2994882777a108e780f8f6446
james karui <jameskarui@jamess-MacBook-Pro.local>
2024-09-25 09:10:54 -0500

    navbar and table of contents

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <h1>Home World</h1>
             <div><Link to="/">Home</Link></div>
             <div><Link to="/create">Create</Link></div>
             <div><Link to="/stories">Stories</Link></div>
