211a9534f96a4a27b8b4863f8bc3c1477e6cb0dd
sageaadam <sageadeleadam@gmail.com>
2024-09-25 19:12:34 -0500

    addd css and 4D cycle

---
+++ client/src/pages/create.jsx
@@ -0,0 +1,36 @@
+import './4DcycleStyle.css';
+export function Create() {
+    return (
+        <>
+            <h1>Create</h1>
+            <div className="s4dcycle">{/*This is the 4D cycle bar*/}
+                <h2>The 4D Cycle</h2>
+                <table>
+                    <tr>
+                <div className= "gallery">
+                <p1>Discern</p1>
+                            <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
+                            <div className= "button1">learnMore</div>
+                </div>
+                <div className= "gallery">
+                <p1>Discern</p1>
+                            <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
+                            <div className= "button1">learnMore</div>
+                </div>
+                <div className= "gallery">
+                <p1>Discern</p1>
+                            <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
+                            <div className= "button1">learnMore</div>
+                </div>
+                <div className= "gallery">
+                <p1>Discern</p1>
+                    <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
+                    <div className= "button1">learnMore</div>
+                </div>
+                </tr>
+                </table>
+                <p1>To effectively lead projects using the 4D cycle...</p1>
+            </div>
+        </>
+    )
+}
---
+++ client/src/pages/help.jsx
@@ -0,0 +1,7 @@
+export function Help() {
+    return (
+        <>
+            <h1>Help</h1>
+        </>
+    )
+}
