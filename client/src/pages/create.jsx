dfb92c9c13f852548b972bed94b3ca1a1fd01bbd
sageaadam <sageadeleadam@gmail.com>
2024-09-27 13:52:37 -0500

    4D cycle bar with descriptions.

--- client/src/pages/create.jsx
+++ client/src/pages/create.jsx
@@ -5,30 +5,28 @@ export function Create() {
             <h1>Create</h1>
             <div className="s4dcycle">{/*This is the 4D cycle bar*/}
                 <h2>The 4D Cycle</h2>
-                <table>
-                    <tr>
-                <div className= "gallery">
-                <p1>Discern</p1>
-                            <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
-                            <div className= "button1">learnMore</div>
-                </div>
-                <div className= "gallery">
-                <p1>Discern</p1>
-                            <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
-                            <div className= "button1">learnMore</div>
-                </div>
-                <div className= "gallery">
-                <p1>Discern</p1>
-                            <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
-                            <div className= "button1">learnMore</div>
-                </div>
-                <div className= "gallery">
-                <p1>Discern</p1>
-                    <p>Follow the leading of the Holy Spirit to pursue a solution.</p>
-                    <div className= "button1">learnMore</div>
-                </div>
-                </tr>
-                </table>
+                <table><tr>
+                    <div className= "gallery">
+                        <p1>Disover</p1>
+                            <p>Reorient to see, through the lens of Christ, those most affected by the problem</p>
+                            <div className= "button1">Learn More</div>
+                    </div>
+                    <div className= "gallery">
+                        <p1>Discern</p1>
+                            <p>Involve the wisdom of God through the Holy Spirit, testing and refining our approach continually,</p>
+                            <div className= "button1">Learn More</div>
+                    </div>
+                     <div className= "gallery">
+                        <p1>Develop</p1>
+                            <p>Recognize that God establishes our steps to develop the solution, and co-develop with the Holy Spirit.</p>
+                            <div className= "button1">Learn More</div>
+                    </div>
+                    <div className= "gallery">
+                        <p1>Demonstrate</p1>
+                            <p>Redefine impact as friendship compounded by time and measure and demonstrate our impact redemptively.</p>
+                            <div className= "button1">Learn More</div>
+                    </div>
+                </tr></table>
                 <p1>To effectively lead projects using the 4D cycle...</p1>
             </div>
         </>
