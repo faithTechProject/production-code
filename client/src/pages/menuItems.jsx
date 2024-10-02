f11e4965c17c7262738a8790097c6c1aa4bef554
james karui <jameskarui@jamess-MacBook-Pro.local>
2024-10-02 09:40:25 -0500

    finding drop down code

---
+++ client/src/pages/menuItems.jsx
@@ -0,0 +1,27 @@
+// menuItems.js
+export const overviewItems = [
+    { label: 'Project', path: '/project' },
+    { label: 'Team', path: '/team' },
+    { label: 'Problem', path: '/problem' },
+  ];
+export const discoverItems = [
+    { label: 'Project', path: '/project' },
+    { label: 'Team', path: '/team' },
+    { label: 'Problem', path: '/problem' },
+  ];
+export const discernItems = [
+    { label: 'Project', path: '/project' },
+    { label: 'Team', path: '/team' },
+    { label: 'Problem', path: '/problem' },
+  ];
+export const developItems = [
+    { label: 'Project', path: '/project' },
+    { label: 'Team', path: '/team' },
+    { label: 'Problem', path: '/problem' },
+  ];
+  
+  export const demonstrateItems = [
+    { label: 'Option 1', path: '/option1' },
+    { label: 'Option 2', path: '/option2' },
+  ];
+  
